32c280cdd55581495f9d29245f58d046e6d7f0fc
oberstrike <markus.juergens@gmx.de>
2020-10-06 14:09:46 +0200

    Update 1.0.1 test

--- settings.gradle.kts
+++
@@ -1,4 +0,0 @@
-rootProject.name = "test-framework"
-val junitJupiterVersion = "5.4.2"
-include("testcontainer")
-include("quarkus")
--- testcontainer/src/main/kotlin/com/maju/postgres/KPostgreSQLContainer.kt
+++
@@ -1,7 +0,0 @@
-package com.maju.postgres
-
-import org.testcontainers.containers.PostgreSQLContainer
-
-open class KPostgreSQLContainer(image: String = "postgres") : PostgreSQLContainer<KPostgreSQLContainer>(image)
-
-
